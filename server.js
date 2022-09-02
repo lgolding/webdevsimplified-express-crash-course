899c23dbbd5dfc68a615c9b430ca7485208b206c
Larry Golding <larrygolding@comcast.net>
2022-09-02 16:14:44 -0700

    Demonstrate res.json.

--- server.js
+++ server.js
@@ -5,5 +5,7 @@ app.listen(port, () => console.log(`Listening on port ${port}...`));
 
 app.get("/", (req, res) => {
   console.log(`Called GET on ${req.url}`);
-  res.status(500).send(`Error ${res.statusCode} from GET on ${req.url}`);
+  res.json({
+    message: `Some data from GET on ${req.url}`,
+  });
 });
