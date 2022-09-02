77037d75d34b321700b0dc9e8f3a3308d2325e96
Larry Golding <larrygolding@comcast.net>
2022-09-02 16:02:08 -0700

    Start the express server.

--- server.js
+++ server.js
@@ -1,1 +1,4 @@
-console.log("Hello, nodemon!");
+const express = require("express");
+const app = express();
+const port = 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
