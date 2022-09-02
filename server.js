9b3ff0972fe2736d521ef065f866f9ae63c1819c
Larry Golding <larrygolding@comcast.net>
2022-09-02 16:06:38 -0700

    Handle GET /.

--- server.js
+++ server.js
@@ -2,3 +2,9 @@ const express = require("express");
 const app = express();
 const port = 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
+
+app.get("/", (req, res) => {
+  console.log(`Called GET on ${req.url}`);
+  res.send(`Hello from GET on ${req.url}`);
+  req.url;
+});
