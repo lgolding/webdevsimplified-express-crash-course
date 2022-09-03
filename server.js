91e672a15c363e901ccdd1b844c65437b76cb16c
Larry Golding <larrygolding@comcast.net>
2022-09-02 17:20:15 -0700

    Pass parameters to the index view.

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = 3000;
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", { text: "World" });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}...`));
