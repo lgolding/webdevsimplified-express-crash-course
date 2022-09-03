61f636b2db1962ba1be5f109e99b7f81056c599d
Larry Golding <larrygolding@comcast.net>
2022-09-02 17:17:17 -0700

    Demonstrate rendering HTML with ejs view engine.

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  console.log(`Called GET on ${req.url}`);
-  res.json({
-    message: `Some data from GET on ${req.url}`,
-  });
+  res.render("index");
 });
+
+app.listen(port, () => console.log(`Listening on port ${port}...`));
